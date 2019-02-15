2eef884bba36e3c453c2cd99b9307f1ab59de866
shang <917058965@qq.com>
2019-02-15 16:38:10 +0800

    add http

---
+++ include/dev.h
@@ -0,0 +1,11 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+//
+    extern char *http_get(char *url);
+    extern char *http_post(char *url, char *contentType, char *body);
+//
+#ifdef __cplusplus
+}
+#endif
